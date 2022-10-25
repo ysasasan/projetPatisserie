511554d94aab6f69a12f95c95b53f273f4ab3edb
Etienne Ferrandi <72975677+EtienneFerrandi@users.noreply.github.com>
2022-10-25 11:09:01 +0200

    CRUD Patisserie

--- Back/projetPatisserie/src/main/java/ajc/fr/thales/projetPatisserie/repository/patisserieRepository.java
+++ Back/projetPatisserie/src/main/java/ajc/fr/thales/projetPatisserie/repository/patisserieRepository.java
@@ -1,5 +1,13 @@
 package ajc.fr.thales.projetPatisserie.repository;
 
-public interface patisserieRepository {
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ajc.fr.thales.projetPatisserie.model.Patisserie;
+
+public interface patisserieRepository extends JpaRepository<Patisserie, Long>{
+	
 
 }
--- Back/projetPatisserie/src/main/java/ajc/fr/thales/projetPatisserie/service/patisserieService.java
+++ Back/projetPatisserie/src/main/java/ajc/fr/thales/projetPatisserie/service/patisserieService.java
@@ -1,5 +1,42 @@
 package ajc.fr.thales.projetPatisserie.service;
 
+import java.util.Objects;
+import java.util.Optional;
+
+import javax.management.relation.RelationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ajc.fr.thales.projetPatisserie.model.Patisserie;
+import ajc.fr.thales.projetPatisserie.repository.patisserieRepository;
+
+@Service
 public class patisserieService {
 
+	@Autowired
+	patisserieRepository pr;
+	
+	
+	public Optional<Patisserie> getById(Long id) {
+		return pr.findById(id);
+	}
+	
+	public Patisserie updatePatisserie(Patisserie p) throws Exception {
+		if (Objects.isNull(p)) {
+			throw new IllegalArgumentException("patisserie could not be null");
+		}
+		if(Objects.isNull(p.getId())) {
+			throw new Exception("no id");
+		}
+		return pr.save(p);
+	}
+	
+	public void deleteClientbyId(Long id) throws RelationException {
+		try {
+			pr.deleteById(id);
+		} catch (Exception e) {
+			throw new RelationException("id ["+id+"] does not exist yet");
+		}
+	}
 }
